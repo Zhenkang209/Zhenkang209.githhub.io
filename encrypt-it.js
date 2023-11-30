69529265b9d4c430289e54f1319bda6c7291e7a1
Zhenkang209 <124333517+Zhenkang209@users.noreply.github.com>
2023-11-29 17:56:02 -0800

    Update encrypt-it.js

--- encrypt-it.js
+++ encrypt-it.js
@@ -1,58 +1,53 @@
-// Part I:
 window.addEventListener("load", function () {
-  console.log("Window loaded!");
-});
-// Part II:
-function handleClick() {
-  console.log("Button clicked!");
-}
-window.addEventListener("load", function () {
-  const encryptButton = document.getElementById("encrypt-it");
-  encryptButton.addEventListener("click", handleClick);
-});
-// Part III:
-function handleReset() {
-  const inputTextArea = document.getElementById("input-text");
-  inputTextArea.value = ""; // Clear out the input text area
-}
-window.addEventListener("load", function () {
-  const resetButton = document.getElementById("reset");
-  resetButton.addEventListener("click", handleReset);
-});
-// Part IV:
-function handleEncrypt() {
-  const inputText = document.getElementById("input-text").value;
-  const encryptedText = shiftCipher(inputText);
-  displayEncryptedText(encryptedText);
-}
+  console.log("Window loaded!"); // Part I:
 
-function shiftCipher(text) {
-  let result = "";
-
-  for (let i = 0; i < text.length; i++) {
-    let charCode = text.charCodeAt(i);
-
-    // Check if the character is a lowercase letter
-    if (charCode >= 97 && charCode <= 122) {
-      charCode = ((charCode - 97 + 1) % 26) + 97;
-    }
-    // Check if the character is an uppercase letter
-    else if (charCode >= 65 && charCode <= 90) {
-      charCode = ((charCode - 65 + 1) % 26) + 65;
-    }
-
-    result += String.fromCharCode(charCode);
+  function handleClick() {
+    console.log("Button clicked!"); // Part II:
   }
 
-  return result;
-}
-
-function displayEncryptedText(encryptedText) {
-  const resultArea = document.getElementById("result");
-  resultArea.textContent = encryptedText;
-}
-
-window.addEventListener("load", function () {
   const encryptButton = document.getElementById("encrypt-it");
-  encryptButton.addEventListener("click", handleEncrypt);
+  encryptButton.addEventListener("click", handleClick);
+
+  function handleReset() {
+    const inputTextArea = document.getElementById("input-text");
+    inputTextArea.value = ""; // Part III:
+  }
+
+  const resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", handleReset);
+
+  function handleEncrypt() {
+    const inputText = document.getElementById("input-text").value;
+    const encryptedText = shiftCipher(inputText); // Part IV:
+    displayEncryptedText(encryptedText);
+  }
+
+  function shiftCipher(text) {
+    let result = "";
+
+    for (let i = 0; i < text.length; i++) {
+      let charCode = text.charCodeAt(i);
+
+      if (charCode >= 97 && charCode <= 122) {
+        charCode = ((charCode - 97 + 1) % 26) + 97;
+      } else if (charCode >= 65 && charCode <= 90) {
+        charCode = ((charCode - 65 + 1) % 26) + 65;
+      }
+
+      result += String.fromCharCode(charCode);
+    }
+
+    return result;
+  }
+
+  function displayEncryptedText(encryptedText) {
+    const resultArea = document.getElementById("result");
+    resultArea.textContent = encryptedText;
+  }
+
+  // Function to log a message to the console when the "Encrypt-It!" button is clicked
+  const encryptItButton = document.getElementById("encrypt-it");
+  encryptItButton.addEventListener("click", function () {
+    console.log("Encrypt-It! button clicked!");
+  });
 });
