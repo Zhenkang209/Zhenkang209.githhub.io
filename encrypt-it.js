2b552543cf2938c924b3474ae6e6cf9170f20bd2
Zhenkang209 <124333517+Zhenkang209@users.noreply.github.com>
2023-11-29 18:12:47 -0800

    Update encrypt-it.js

--- encrypt-it.js
+++ encrypt-it.js
@@ -24,28 +24,22 @@ window.addEventListener("load", function () {
 
   function shiftCipher(text) {
     let result = "";
-
     for (let i = 0; i < text.length; i++) {
       let charCode = text.charCodeAt(i);
-
       if (charCode >= 97 && charCode <= 122) {
         charCode = ((charCode - 97 + 1) % 26) + 97;
       } else if (charCode >= 65 && charCode <= 90) {
         charCode = ((charCode - 65 + 1) % 26) + 65;
       }
-
       result += String.fromCharCode(charCode);
     }
-
     return result;
   }
-
   function displayEncryptedText(encryptedText) {
     const resultArea = document.getElementById("result");
     resultArea.textContent = encryptedText;
   }
-
-  // Function to log a message to the console when the "Encrypt-It!" button is clicked
+  //
   const encryptItButton = document.getElementById("encrypt-it");
   encryptItButton.addEventListener("click", function () {
     console.log("Encrypt-It! button clicked!");
