048f476a0b9a7615ae0ac7e153b0530dd4162683
Zhenkang209 <124333517+Zhenkang209@users.noreply.github.com>
2023-11-29 18:17:00 -0800

    Update encrypt-it.js

--- encrypt-it.js
+++ encrypt-it.js
@@ -23,18 +23,22 @@ window.addEventListener("load", function () {
   }
 
   function shiftCipher(text) {
-    let result = "";
-    for (let i = 0; i < text.length; i++) {
-      let charCode = text.charCodeAt(i);
-      if (charCode >= 97 && charCode <= 122) {
-        charCode = ((charCode - 97 + 1) % 26) + 97;
-      } else if (charCode >= 65 && charCode <= 90) {
-        charCode = ((charCode - 65 + 1) % 26) + 65;
-      }
-      result += String.fromCharCode(charCode);
+  const shiftAmount = 3; // You can change this value to shift by a different amount
+
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    let charCode = text.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = ((charCode - 65 + shiftAmount) % 26) + 65;
+    } else if (charCode >= 97 && charCode <= 122) {
+      charCode = ((charCode - 97 + shiftAmount) % 26) + 97;
     }
-    return result;
+    result += String.fromCharCode(charCode);
   }
+  return result;
+}
+
   function displayEncryptedText(encryptedText) {
     const resultArea = document.getElementById("result");
     resultArea.textContent = encryptedText;
